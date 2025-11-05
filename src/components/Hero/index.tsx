f3b112136013423a291cd7c5f63adf64f221c556
RemiJolian <rbagheri85@gmail.com>
2025-11-05 13:45:36 +0330

    modify home page color-title

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <>
       <section
         id="home"
-        className="relative overflow-hidden bg-primary pt-[120px] md:pt-[130px] lg:pt-[160px]"
+        className="relative overflow-hidden bg-[#0D9488] pt-[120px] md:pt-[130px] lg:pt-[160px]"
       >
         <div className="container">
           <div className="-mx-4 flex flex-wrap items-center">
@@ -16,7 +16,7 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
-                  Open-source SaaS Starter Kit and Boilerplate for Next.js
+                  Run the NF-Core Pipelines Fast and Easy
                 </h1>
                 <p className="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
                   Next.js SaaS Boilerplate and Starter Kit designed and built
